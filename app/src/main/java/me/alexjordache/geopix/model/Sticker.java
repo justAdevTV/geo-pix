8a75124509714306fcea4b5a2488f33448a4d636
Alex <alexonej@gmail.com>
2016-04-30 11:44:18 -0700

    started user model

---
+++ app/src/main/java/me/alexjordache/geopix/model/Sticker.java
@@ -0,0 +1,7 @@
+package me.alexjordache.geopix.model;
+
+/**
+ * Created by Alex on 4/30/2016.
+ */
+public class Sticker {
+}
---
+++ app/src/main/java/me/alexjordache/geopix/model/User.java
@@ -0,0 +1,54 @@
+package me.alexjordache.geopix.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Alex on 4/30/2016.
+ */
+public class User {
+
+    // User Fields
+
+    private String userName;
+    private String password;
+    private ArrayList<String> stickers;
+    
+
+    // Constructor
+
+    public User(String userName, String password, ArrayList<String> stickers ) {
+        userName = this.userName;
+        password = this.password;
+        stickers = this.stickers;
+    }
+
+    // Getters & Setters
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ArrayList<String> getStickers() {
+        return stickers;
+    }
+
+    public void setStickers(ArrayList<String> stickers) {
+        this.stickers = stickers;
+    }
+
+//    public static void doLogin()
+
+}
