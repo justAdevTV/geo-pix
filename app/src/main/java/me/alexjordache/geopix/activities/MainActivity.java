f33368643f166a05cd832b72bb89afcb79c482ce
Alex <alexonej@gmail.com>
2016-05-01 02:18:51 -0700

    add more fields for main activity

--- app/src/main/java/me/alexjordache/geopix/activities/MainActivity.java
+++ app/src/main/java/me/alexjordache/geopix/activities/MainActivity.java
@@ -1,7 +1,10 @@
 package me.alexjordache.geopix.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import me.alexjordache.geopix.R;
 
@@ -13,5 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void goToNFC(View view) {
 
+        TextView userNameView = (TextView)findViewById(R.id.main_userName);
+        TextView stickerCollectionView = (TextView)findViewById(R.id.yourSticker);
+        TextView stickersFoundView = (TextView)findViewById(R.id.stickersFound);
+
+        String userName = userNameView.getText().toString();
+        Integer stickersCollection = Integer.parseInt(stickerCollectionView.getText().toString());
+        Integer stickersFound = Integer.parseInt(stickersFoundView.getText().toString());
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+
+    }
 }
