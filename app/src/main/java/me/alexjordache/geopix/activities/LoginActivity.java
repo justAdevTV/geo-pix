a1cccbd32a7d908923257fc96c51714a7f8d8e60
Jason Wong <jason_19960903@hotmail.com>
2016-05-01 01:26:16 -0700

    http request to signup

--- app/src/main/java/me/alexjordache/geopix/activities/LoginActivity.java
+++ app/src/main/java/me/alexjordache/geopix/activities/LoginActivity.java
@@ -6,7 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 
+import java.io.IOException;
+
+
 import me.alexjordache.geopix.R;
+import okhttp3.Call;
+import okhttp3.Headers;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.Callback;
+
 
 /**
  * Created by Alex on 4/30/2016.
@@ -23,15 +35,49 @@ public class LoginActivity extends AppCompatActivity {
 
     public void doLogin(View view) {
 
+
+        try {
+            run();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+
+    private final OkHttpClient client = new OkHttpClient();
+    public void run() throws IOException {
         EditText userNameField = (EditText)findViewById(R.id.initial_username);
         EditText passwordField = (EditText)findViewById(R.id.initial_password);
 
         String userName = userNameField.getText().toString();
         String password = passwordField.getText().toString();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        RequestBody body = RequestBody.create(mediaType, "username=" + userName + "&password=" + password + "&=");
+        Request request = new Request.Builder()
+                .url("http://geopix.azurewebsites.net/api/signup")
+                .post(body)
+                .addHeader("cache-control", "no-cache")
+                .addHeader("postman-token", "7d0fdc07-ec55-5a02-c423-7f16883351bf")
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
 
+            @Override public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
+                Headers responseHeaders = response.headers();
+                for (int i = 0, size = responseHeaders.size(); i < size; i++) {
+                    System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+                }
 
+                System.out.println(response.body().string());
+            }
+        });
     }
 }
